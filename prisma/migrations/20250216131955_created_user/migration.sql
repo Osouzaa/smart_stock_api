7c5825d3c77a0d41df40f441743cfe40762d5151
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-02-16 10:21:34 -0300

    feat: created model user

---
+++ prisma/migrations/20250216131955_created_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'ESTOQUISTA', 'FINANCEIRO');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha_hash" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'ESTOQUISTA',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
